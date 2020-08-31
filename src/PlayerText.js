bd1f1949ea8ccd84596cc9c28b3add5c35e826fb
Weston C. Beecroft <westoncb@gmail.com>
2020-08-31 01:33:03 -0700

    experimenting with ai checkboxes

--- src/PlayerText.js
+++ src/PlayerText.js
@@ -1,12 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
 
-function PlayerText({text, highlight, isPlayer1, boardRightX}) {
+function PlayerText({text, highlight, isPlayer1}) {
+    const [ai, setAI] = useState(!isPlayer1)
 
     return (
         <div 
             className={"player-text " + (isPlayer1 ? "p1-text" : "p2-text") + (highlight ? " text-highlight" : "")}
             >
             {text}
+
+            {/* <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center" }}>
+                <label className="ai-label">A.I.</label>
+                <input 
+                    type="checkbox"
+                    className="ios-switch tinyswitch"
+                    checked={ai}
+                    onChange={e => setAI(!ai)} />
+                <div><div></div></div>
+            </div> */}
         </div>
     )
 }

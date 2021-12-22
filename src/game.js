e8423acc423ca4c285f1de5a123dcb0ae4ac9f02
Weston C. Beecroft <westoncb@gmail.com>
2021-12-22 15:10:43 -0700

    add errorboundary

--- src/game.js
+++ src/game.js
@@ -8,10 +8,41 @@ class Game extends React.Component {
     }
 
     render() {
-        return <Board></Board>
+        return (
+            <ErrorBoundary>
+                <Board></Board>
+            </ErrorBoundary>
+        )
     }
 
     reset() {}
+}
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { error: null, errorInfo: null }
+    }
+
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        this.setState({ errorInfo, error })
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            // You can render any custom fallback UI
+            return (
+                <div className="error-boundary">
+                    <h1>Something went wrong.</h1>
+                    <div>{this.state.error?.toString()}</div>
+                    <div>{this.state.errorInfo.componentStack}</div>
+                </div>
+            )
+        }
+
+        return this.props.children
+    }
 }
 
 export default Game

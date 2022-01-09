858954b039469b7dec5bdd38e153e705640fbf05
Weston C. Beecroft <westoncb@gmail.com>
2022-01-08 21:19:58 -0700

    auto-format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const webpack = require("webpack")
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
     module: {
         rules: [
@@ -14,17 +14,17 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env', "@babel/preset-react"]
-                    }
-                }
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
+                },
             },
             {
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
-                        loader: 'file-loader'
+                        loader: "file-loader",
                     },
                 ],
             },
@@ -32,27 +32,27 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    'style-loader',
+                    "style-loader",
                     // Translates CSS into CommonJS
-                    'css-loader',
+                    "css-loader",
                     // Compiles Sass to CSS
-                    'sass-loader',
+                    "sass-loader",
                 ],
             },
-        ]
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Dara",
             template: __dirname + "/src/index.html",
             file: "index.html",
-            inject: "body"
+            inject: "body",
         }),
         new webpack.HotModuleReplacementPlugin(),
     ],
     devServer: {
-        contentBase: './dist',
+        contentBase: "./dist",
         open: true,
-        hot: true
-    }
-};
+        hot: true,
+    },
+}
